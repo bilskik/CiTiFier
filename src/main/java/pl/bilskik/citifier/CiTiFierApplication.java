fc83ad1dcfeeaa8bfdc5c89497f2776fb94aaaea
bilskik <kbilski77@gmail.com>
2024-05-16 16:24:55 +0200

    init commit

---
+++ src/main/java/pl/bilskik/citifier/CiTiFierApplication.java
@@ -0,0 +1,13 @@
+package pl.bilskik.citifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiTiFierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiTiFierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/bilskik/citifier/CiTiFierApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bilskik.citifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiTiFierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
