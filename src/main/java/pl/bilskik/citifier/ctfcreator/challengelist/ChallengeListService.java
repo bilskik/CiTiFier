f4973217b524c0d0c5ef09f1478e67d1f3fb25ce
bilskik <kbilski77@gmail.com>
2024-10-19 18:30:35 +0200

    feat: move allowed docker_compose filenames to external config; remove mocked filepaths

--- src/main/java/pl/bilskik/citifier/ctfcreator/challengelist/ChallengeListService.java
+++ src/main/java/pl/bilskik/citifier/ctfcreator/challengelist/ChallengeListService.java
@@ -2,6 +2,7 @@ package pl.bilskik.citifier.ctfcreator.challengelist;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.bilskik.citifier.ctfcreator.challenge.ChallengeDTO;
 import pl.bilskik.citifier.ctfcreator.dockebuilder.BuildDockerContainers;
@@ -11,6 +12,7 @@ import pl.bilskik.citifier.ctfcreator.kubernetes.K8sResourceContext;
 import pl.bilskik.citifier.ctfcreator.kubernetes.K8sResourceManager;
 import pl.bilskik.citifier.ctfdomain.service.ChallengeDao;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -21,8 +23,13 @@ import java.util.regex.Pattern;
 @Slf4j
 public class ChallengeListService {
 
-    private final static String EMPTY = "";
     private final static String K8S_LABEL_REGEX = "^([A-Za-z0-9]([-A-Za-z0-9_.]{0,55}[A-Za-z0-9])?)?$"; //max 55 characters long for additional values
+
+    @Value("${repo.base-file-path}")
+    private String baseFilePath;
+
+    @Value("${repo.allowed-docker-compose-file-names}")
+    private List<String> DOCKER_COMPOSE;
 
     private final ChallengeDao challengeDao;
     private final K8sResourceManager k8SResourceManager;
@@ -37,16 +44,19 @@ public class ChallengeListService {
         return challengeDao.findAll();
     }
 
-    public void parseComposeAndDeployApp() {
-        dockerContainers.build("D:\\CLONED_REPO_CITIFIER\\secure-notes-app");
-        K8sResourceContext resourceContext = loadAppDataToK8sResourceContext();
-        DockerCompose compose = dockerComposeParserManager.parse("D:\\CLONED_REPO_CITIFIER\\secure-notes-app\\compose.yaml");
+    public void parseComposeAndDeployApp(Long challengeId) {
+        String repoName = challengeDao.findRepoNameByChallengeId(challengeId);
+        String baseFilePathWithRepo = baseFilePath + "\\" + repoName;
+        K8sResourceContext resourceContext = loadAppDataToK8sResourceContext(challengeId);
+        DockerCompose compose = dockerComposeParserManager.parse(baseFilePathWithRepo + provideDockerComposeYamlName(baseFilePathWithRepo));
         resourceContext.setDockerCompose(compose);
+
+        dockerContainers.build(baseFilePathWithRepo);
 
         k8SResourceManager.deploy(resourceContext);
     }
 
-    private K8sResourceContext loadAppDataToK8sResourceContext() {
+    private K8sResourceContext loadAppDataToK8sResourceContext(Long challengeId) {
 //        ChallengeAppDataDTO appData = challengeDao.findChallengeAppDataDTOByChallengeId(1L);
 
 //        return K8sResourceContext.builder()
@@ -78,5 +88,15 @@ public class ChallengeListService {
     private boolean isValidLabel(String labelValue) {
         Pattern pattern = Pattern.compile(K8S_LABEL_REGEX);
         return pattern.matcher(labelValue).matches();
+    }
+
+    private String provideDockerComposeYamlName(String baseFilePath) {
+        for(var fileName: DOCKER_COMPOSE) {
+            File file = new File(baseFilePath + fileName);
+            if(file.exists()) {
+                return fileName;
+            }
+        }
+        throw new IllegalArgumentException("NIE MAAAA");
     }
 }
