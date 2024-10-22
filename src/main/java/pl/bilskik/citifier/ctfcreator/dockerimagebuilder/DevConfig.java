6dc878b4d3ecb51a597337d309a27008ae38e139
bilskik <kbilski77@gmail.com>
2024-10-22 22:56:43 +0200

    refactor: remove unused imports/configurations

--- src/main/java/pl/bilskik/citifier/ctfcreator/dockerimagebuilder/DevConfig.java
+++ src/main/java/pl/bilskik/citifier/ctfcreator/dockerimagebuilder/DevConfig.java
@@ -1,12 +1,10 @@
 package pl.bilskik.citifier.ctfcreator.dockerimagebuilder;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-//@EnableConfigurationProperties(DockerShellProperties.class)
 @Profile("dev")
 public class DevConfig {
 
